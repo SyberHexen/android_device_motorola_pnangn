1287fd6b8e703385f67b3b4e186817975fad927a
Andrew Hexen <SyberHexen@gmail.com>
2025-05-16 13:58:18 -0700

    pnangn: Initial rebranding from fogo
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I2a3a8918c63d9b64d3d604acaddc76f860aa0c11

--- device.mk
+++ device.mk
@@ -81,4 +81,4 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Inherit from vendor blobs
-$(call inherit-product, vendor/motorola/fogo/fogo-vendor.mk)
+$(call inherit-product, vendor/motorola/pnangn/pnangn-vendor.mk)
--- lineage_pnangn.mk
+++ lineage_pnangn.mk
@@ -8,21 +8,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from fogos device
-$(call inherit-product, device/motorola/fogo/device.mk)
+# Inherit from pnangns device
+$(call inherit-product, device/motorola/pnangn/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fogo
-PRODUCT_DEVICE := fogo
+PRODUCT_NAME := lineage_pnangn
+PRODUCT_DEVICE := pnangn
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g 5G - 2024
+PRODUCT_MODEL := moto g 5G - 2023
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fogo_g-user 15 V1UFN35H.193-20 3119bc-9f63b release-keys MV-304" \
-    BuildFingerprint=motorola/fogo_g/fogo:15/V1UFN35H.193-20/3119bc-9f63b:user/release-keys \
-    DeviceProduct=fogo_g
+    BuildDesc="pnangn_g-user 14 U1UFNS34.41-98-3-16 31deb6-790f4 release-keys MUR1-0.252" \
+    BuildFingerprint=motorola/pnangn_g/pnangn:14/U1UFNS34.41-98-3-16/31deb6-790f4:user/release-keys \
+    DeviceProduct=pnangn_g
