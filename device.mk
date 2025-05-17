4059db56bab70d12ca28f4ec1022224a732c1447
Andrew Hexen <SyberHexen@gmail.com>
2025-05-17 08:22:50 -0700

    pnangn: Initial device specifics
    * From: 'U1TPNS34.26-78-3-17'
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: If6ae369d7bac65966852aa1c2dc224dddbe130b9

--- device.mk
+++ device.mk
@@ -36,8 +36,10 @@ PRODUCT_PACKAGES += \
     firmware_aw_cali.bin_symlink
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_TMUS.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_TMUS.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
